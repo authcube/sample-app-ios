ff067709db8a0d8c5d958abb3d64bff7ecc24dd4
Alex Eduardo Chiaranda <aechiara@gmail.com>
2025-01-08 16:39:39 -0300

    Enroll TOTP

--- AppSample/strong/TotpView.swift
+++ AppSample/strong/TotpView.swift
@@ -20,11 +20,208 @@ struct TotpView: View {
     
     @State private var seedAvailable: Bool = false
     
+    @State private var enrollmentInfo = ""
+    @State private var errorMessage: String = ""
     
     /// -- functions
 
     private func hasSeed() -> Bool {
         return viewModel.appDelegate.authfySdk.hasSeed()
+    }
+    
+    // AppStorage
+    @AppStorage("url_idp") private var urlIdp: String = ""
+    @AppStorage("client_secret") private var clientSecret: String = ""
+    
+    
+    func doEnrollment() {
+        errorMessage = ""
+        
+        let fixedUrl = urlIdp.components(separatedBy: "/").dropLast().joined(separator: "/")
+        let enrollmentEndpoint = URL(string: "\(fixedUrl)/mfa/totp/enrollment")!
+        
+        viewModel.appDelegate.getAuthState()!.performAction() { (accessToken, idToken, error) in
+            
+            if error != nil  {
+                print("Error fetching fresh tokens: \(error?.localizedDescription ?? "Unknown error")")
+                return
+            }
+            guard let accessToken = accessToken else {
+                return
+            }
+            
+            
+            let jsonString = "{\"verbose\": false}"
+            guard let jsonData = jsonString.data(using: .utf8) else {
+                print("Erro ao criar dados JSON")
+                return
+            }
+            
+            // Add Bearer token to request
+            var urlRequest = URLRequest(url: enrollmentEndpoint)
+            urlRequest.httpMethod = "POST"
+            urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            urlRequest.allHTTPHeaderFields = ["Authorization": "Bearer \(accessToken)"]
+            urlRequest.httpBody = jsonData
+            
+            // Perform request...
+            let session = URLSession.shared
+            let task = session.dataTask(with: urlRequest) { data, response, error in
+                // Verifique se houve algum erro
+                if let error = error {
+                    print("Erro ao fazer a solicitação: \(error)")
+                    return
+                }
+                
+                // Verificar a resposta HTTP e status code
+                if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 201 {
+                    // Tratar a resposta com sucesso
+                    if let data = data {
+                        // Supondo que a resposta seja um JSON
+                        do {
+                            // Tentar decodificar a resposta JSON, supondo uma estrutura básica
+                            let responseObject = try JSONSerialization.jsonObject(with: data, options: [])
+                            print("Resposta JSON: \(responseObject)")
+                            
+                            if let r = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+                               let contents = r["contents"] as? [[String: Any]] {
+                                
+                                // Procurando pelo objeto correto no array 'contents'
+                                for content in contents {
+                                    if let rel = content["rel"] as? [String],
+                                       rel.contains("urn:mfao:totp:enrollment:data"),
+                                       let values = content["values"] as? [String],
+                                       var totpURL = values.first {
+                                        // Agora 'totpURL' contém o valor desejado
+                                        enrollmentInfo = totpURL
+                                        print("TOTP URL: \(totpURL)")
+                                        totpURL = totpURL.replacingOccurrences(of: "-", with: "")
+                                        try viewModel.appDelegate.authfySdk.setSeed(data: totpURL)
+                                        seedAvailable = viewModel.appDelegate.authfySdk.hasSeed()
+                                        self.codeNumber = try viewModel.appDelegate.authfySdk.generateTOTP()
+                                    }
+                                }
+                            }
+                            
+                        } catch {
+                            print("Erro ao decodificar JSON: \(error)")
+                            enrollmentInfo = ""
+                            codeNumber = ""
+                        }
+                    }
+                } else {
+                    // Lidar com resposta HTTP diferente de 200 OK
+                    if let httpResponse = response as? HTTPURLResponse {
+                        print("HTTP Status Code: \(httpResponse.statusCode)")
+                        enrollmentInfo = ""
+                        errorMessage = "Status Code: \(httpResponse.statusCode)"
+                        
+                        if httpResponse.statusCode == 409 {
+                            errorMessage = "Enrollment already completed, delete the existing enrollment before try to enroll again"
+                        }
+                        
+                    }
+                    
+                    codeNumber = ""
+
+                }
+                
+                seedAvailable = viewModel.appDelegate.authfySdk.hasSeed()
+            }
+            
+            // Iniciar a tarefa
+            task.resume()
+            
+        }
+        
+    }
+    
+    func doDeleteEnrollment() {
+        errorMessage = ""
+        
+        //        let edeleteEndpoint = URL(string:"https://newpst.authfy.tech/demo/mfa/totp/delete")!
+        
+        let fixedUrl = urlIdp.components(separatedBy: "/").dropLast().joined(separator: "/")
+        let deleteEndpoint = URL(string: "\(fixedUrl)/mfa/totp/delete")!
+        
+        viewModel.appDelegate.getAuthState()!.performAction() { (accessToken, idToken, error) in
+            
+            if error != nil  {
+                print("Error fetching fresh tokens: \(error?.localizedDescription ?? "Unknown error")")
+//                changeAuthenticationState(false)
+//                isPresented = false
+                return
+            }
+            guard let accessToken = accessToken else {
+//                isPresented = false
+                return
+            }
+            
+            
+            let jsonString = "{\"verbose\": false}"
+            guard let jsonData = jsonString.data(using: .utf8) else {
+                print("Erro ao criar dados JSON")
+                return
+            }
+            
+            // Add Bearer token to request
+            var urlRequest = URLRequest(url: deleteEndpoint)
+            urlRequest.httpMethod = "POST"
+            urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            urlRequest.allHTTPHeaderFields = ["Authorization": "Bearer \(accessToken)"]
+            urlRequest.httpBody = jsonData
+            
+            // Perform request...
+            let session = URLSession.shared
+            let task = session.dataTask(with: urlRequest) { data, response, error in
+                // Verifique se houve algum erro
+                if let error = error {
+                    print("Erro ao fazer a solicitação: \(error)")
+                    return
+                }
+                
+                // Verificar a resposta HTTP e status code
+                if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
+                    // Tratar a resposta com sucesso
+                    
+                    do {
+                        try viewModel.appDelegate.authfySdk.deleteSeed()
+                        seedAvailable = viewModel.appDelegate.authfySdk.hasSeed()
+                    } catch {
+                        print("Error deleting Seed")
+                    }
+                    
+                    if let data = data {
+                        // Supondo que a resposta seja um JSON
+                        do {
+                            // Tentar decodificar a resposta JSON, supondo uma estrutura básica
+                            let responseObject = try JSONSerialization.jsonObject(with: data, options: [])
+                            print("Resposta JSON: \(responseObject)")
+                            enrollmentInfo = ""
+                        } catch {
+                            print("Erro ao decodificar JSON: \(error)")
+                            enrollmentInfo = ""
+                        }
+                    }
+                } else {
+                    // Lidar com resposta HTTP diferente de 200 OK
+                    if let httpResponse = response as? HTTPURLResponse {
+                        print("HTTP Status Code: \(httpResponse.statusCode)")
+                        enrollmentInfo = ""
+                        errorMessage = "Status Code: \(httpResponse.statusCode)"
+                    }
+                }
+                
+                seedAvailable = viewModel.appDelegate.authfySdk.hasSeed()
+                codeNumber = ""
+
+            }
+            
+            // Iniciar a tarefa
+            task.resume()
+            
+        }
+        
     }
 
 
@@ -102,7 +299,7 @@ struct TotpView: View {
                     if !seedAvailable {
                         // Enroll Button
                         Button(action: {
-                            
+                            doEnrollment()
                             self.otpType = "TOTP"
                         }) {
                             RoundedRectangle(cornerRadius: 10)
@@ -125,7 +322,8 @@ struct TotpView: View {
                     } else {
                         // UnEnroll Button
                         Button(action: {
-                            
+                            doDeleteEnrollment()
+                            self.otpType = "None"
                         }) {
                             RoundedRectangle(cornerRadius: 10)
                                 .fill(Color(hex: "#333333")!)
